2b30cbf5c4100bf75d66e997cb048817c9bffea1
Daniel León Paulin <dleon29@alumnos.uaq.mx>
2022-05-26 08:06:47 -0500

    add

--- routes/rutas.js
+++ routes/rutas.js
@@ -19,7 +19,7 @@ router.get('/', function (req, res, next) {
     db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
 });
 
-router.get('/Enfermedad/:nombre', function (req, res, next) {
+router.get('/Emergencia/:nombre', function (req, res, next) {
     var nombre = req.params.nombre;;
 
     db = database.conectar();
@@ -73,62 +73,62 @@ router.get('/Enfermedad/:nombre', function (req, res, next) {
     db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
 });
 
-router.get('/Contacto', function (req, res, next) {
-    var nombre = req.params.nombre;;
+// router.get('/Contacto', function (req, res, next) {
+//     var nombre = req.params.nombre;;
 
-    db = database.conectar();
-    db.query("SELECT * FROM Emergencia WHERE nombre = '" + nombre + "'",
-        function (err, emergencia, fields) {
-            if (err) {
-                console.log(err)
-            } else {
+//     db = database.conectar();
+//     db.query("SELECT * FROM Emergencia WHERE nombre = '" + nombre + "'",
+//         function (err, emergencia, fields) {
+//             if (err) {
+//                 console.log(err)
+//             } else {
 
-                db = database.conectar();
-                db.query("SELECT * FROM  WHERE tratamiento = '" + emergencia[0].id_emergencia + "'",
-                    function (err, tratamiento, fields) {
-                        if (err) {
-                            console.log(err)
-                        } else {
+//                 db = database.conectar();
+//                 db.query("SELECT * FROM  WHERE tratamiento = '" + emergencia[0].id_emergencia + "'",
+//                     function (err, tratamiento, fields) {
+//                         if (err) {
+//                             console.log(err)
+//                         } else {
 
                             
 
-                            db = database.conectar();
-                            db.query("SELECT * FROM  WHERE recomendacion = '" + emergencia[0].id_emergencia + "'",
-                                function (err, recomendacion, fields) {
-                                    if (err) {
-                                        console.log(err)
-                                    } else {
+//                             db = database.conectar();
+//                             db.query("SELECT * FROM  WHERE recomendacion = '" + emergencia[0].id_emergencia + "'",
+//                                 function (err, recomendacion, fields) {
+//                                     if (err) {
+//                                         console.log(err)
+//                                     } else {
 
-                                        res.status(200);
-                                        res.send(recomendacion);
-                                        db = database.conectar();
-                                        db.query("SELECT * FROM  WHERE imagen = '" + emergencia[0].id_emergencia + "'",
-                                            function (err, emergencia, fields) {
-                                                if (err) {
-                                                    console.log(err)
-                                                } else {
-                                                    res.status(200);
+//                                         res.status(200);
+//                                         res.send(recomendacion);
+//                                         db = database.conectar();
+//                                         db.query("SELECT * FROM  WHERE imagen = '" + emergencia[0].id_emergencia + "'",
+//                                             function (err, emergencia, fields) {
+//                                                 if (err) {
+//                                                     console.log(err)
+//                                                 } else {
+//                                                     res.status(200);
 
-                                                    res.json({emergencia: emergencia, tratamiento: tratamiento, recomendacion: recomendacion, imagen: imagen});
-                                                }
-                                            })
-                                        //Terminar conexion
-                                        db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
+//                                                     res.json({emergencia: emergencia, tratamiento: tratamiento, recomendacion: recomendacion, imagen: imagen});
+//                                                 }
+//                                             })
+//                                         //Terminar conexion
+//                                         db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
 
-                                    }
-                                })
-                            //Terminar conexion
-                            db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
-                        }
-                    })
-                //Terminar conexion
-                db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
+//                                     }
+//                                 })
+//                             //Terminar conexion
+//                             db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
+//                         }
+//                     })
+//                 //Terminar conexion
+//                 db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
 
-            }
-        })
-    //Terminar conexion
-    db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
-});
+//             }
+//         })
+//     //Terminar conexion
+//     db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
+// });
 
 module.exports = router;
 
