6489fda36aa98b129decbbc3a145cde48827c72f
guadalupe13141 <60676434+guadalupe13141@users.noreply.github.com>
2022-05-25 19:05:45 -0500

    infoPage

---
+++ src/components/InfoPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+  
+const InfoPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh',
+        color: '#000'
+      }}
+    >
+      <h1>Información</h1>
+    </div>
+  );
+};
+  
+export default InfoPage;
