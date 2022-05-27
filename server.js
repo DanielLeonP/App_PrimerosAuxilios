8f1cdae785848dd3bc99389ebd3da3ce0c30d9b8
Michell Alejandro GV <ale.gv258@gmail.com>
2022-05-27 10:05:26 -0500

    update CORS management

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
-
 const bodyParser = require('body-parser'); 
 const rutas = require('./routes/rutas');
+const cors = require('cors');
 
 var app = express(); 
 //const database = require("./database/database.js");
@@ -10,11 +10,12 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 
 
 app.use('/', rutas);
 
-app.listen(process.env.PORT || 3000, () => { console.log('Servidor Web Iniciado'); });
+app.listen(process.env.PORT || 5000, () => { console.log('Servidor Web Iniciado'); });
 
 module.exports = app;
